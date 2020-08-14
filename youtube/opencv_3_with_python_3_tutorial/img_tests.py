4dfcd690761cf38e2e6e9444c69026e8a6952ecf
Krishna Oza <koza@nvidia.com>
2020-08-14 11:13:17 +0530

    correct img file name

--- youtube/opencv_3_with_python_3_tutorial/img_tests.py
+++ youtube/opencv_3_with_python_3_tutorial/img_tests.py
@@ -1,9 +1,9 @@
 import cv2
 
 def main():
-    imgpath = "C:\\Users\koza\Documents\GitHub\moocs\youtube\opencv_3_with_python_3_tutorial\standard_test_images\lena_color_256.tiff"
+    imgpath = "C:\\Users\koza\Documents\GitHub\moocs\youtube\opencv_3_with_python_3_tutorial\standard_test_images\lena_color_256.tif"
     img = cv2.imread(imgpath)
-    cv2.imgshow("Lean", img)
+    cv2.imshow("Lena", img)
     cv2.waitKey(0)
     cv2.destoryAllWindows()
 
